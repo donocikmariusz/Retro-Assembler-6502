3f60a60af1f76f110e9a12d91188ac6ebc9e582b
Mariusz Donocik <1987mariuszd1987@wp.pl>
2024-01-18 14:54:59 +0100

    Load values to A Register

--- makerom.py
+++ makerom.py
@@ -1,4 +1,15 @@
 rom = bytearray([0xea] * 32768)
 
+rom[0] = 0xa9 #load the next byte from ROM into A Register (A9)
+rom[1] = 0x42 #load value (42) to A Register
+
+#those three bits below says: store the contents of A register at address 6000
+rom[2] = 0x8d   #(8D) - store a value in A Register to an address somewhere in memory
+rom[3] = 0x00
+rom[4] = 0x60
+
+rom[0x7ffc] = 0x00
+rom[0x7ffd] = 0x80
+
 with open("rom.bin", "wb") as out_file:
     out_file.write(rom);
