866b652f70e70628c1e66d4d6dfd0b912312877f
Mariusz Donocik <1987mariuszd1987@wp.pl>
2024-01-19 14:37:48 +0100

    First Assembly Language

--- makerom.py
+++ makerom.py
@@ -1,5 +1,19 @@
-rom = bytearray([0xea] * 32768)
+code = bytearray([
+  0xa9, 0xff,        # lda #$ff
+  0x8d, 0x02, 0x60,  # sta $6002
 
+  0xa9, 0x55,        # lda #$55
+  0x8d, 0x00, 0x60,  # sta $6000
+
+  0xa9, 0xaa,        # lda #$aa
+  0x8d, 0x00, 0x60,  # sta $6000
+
+  0x4c, 0x05, 0x80,  # jmp $8005
+])
+
+rom = code + bytearray([0xea] * (32768 - len(code)))
+
+"""
 rom[0] = 0xa9 #load the next byte from ROM into A Register (A9)
 rom[1] = 0x42 #load value (42) to A Register
 
@@ -7,6 +21,7 @@ rom[1] = 0x42 #load value (42) to A Register
 rom[2] = 0x8d   #(8D) - store a value in A Register to an address somewhere in memory
 rom[3] = 0x00
 rom[4] = 0x60
+"""
 
 rom[0x7ffc] = 0x00
 rom[0x7ffd] = 0x80
